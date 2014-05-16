3744e18534400666e72d641e6863f74158fbfab9
David <david@templatious.org>
2014-05-16 10:42:14 +0300

    all at once

---
+++ adapters/All.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  All.h
+ *
+ *    Description:  All adapters in one include
+ *
+ *        Version:  1.0
+ *        Created:  05/16/2014 10:40:53 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#include <templatious/adapters/StdVector.h>
---
+++ manip.h
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  manip.h
+ *
+ *    Description:  All in one
+ *
+ *        Version:  1.0
+ *        Created:  05/16/2014 10:39:55 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#include <templatious/CollectionManipulator.h>
+#include <templatious/adapters/All.h>
