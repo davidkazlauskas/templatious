878c54187747096cb80f3e3703f6f2cec2d7cf25
David <david@templatious.org>
2014-06-15 19:39:59 +0300

    map adapter carcass

---
+++ MapAdapter.h
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Hash.h
+ *
+ *    Description:  Uniform hash interface
+ *
+ *        Version:  1.0
+ *        Created:  06/15/2014 07:35:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#ifndef HASH_93Z0H2A5
+#define HASH_93Z0H2A5
+
+namespace templatious {
+namespace adapters {
+
+template <class T>
+struct MapAdapter {
+
+
+};
+
+}
+}
+
+
+#endif /* end of include guard: HASH_93Z0H2A5 */
