1077ef380802bc5a3645b5b7c3a5d56fcc8df940
David <david@templatious.org>
2014-07-02 20:06:12 +0300

    Loop SOLID
    need reverse loops in next iter
    Less Equal, More Equal etc. loops are needed
    filled todo list with more features to be added

--- Loop.h
+++ Loop.h
@@ -20,6 +20,7 @@
 #define LOOP_U82GY31R
 
 #include <cstddef>
+#include <assert.h>
 #include <templatious/Utilities.h>
 
 namespace templatious {
@@ -30,15 +31,21 @@ struct LoopL {
     size_t _step;
 
     LoopL(size_t end) : _beg(0), _end(end), _step(1) {}
+    LoopL(size_t beg,size_t end) : _beg(beg), _end(end), _step(1) {
+        assert(beg <= end);
+    }
+
+    LoopL(size_t beg,size_t end,size_t step) : _beg(beg), _end(end), _step(step) {
+        assert(beg <= end);
+        assert(step > 0);
+    }
 
     struct Iter {
-        size_t _beg;
-        size_t _end;
-        size_t _step;
         size_t _count;
+        size_t _step;
 
-        Iter(size_t beg,size_t end,size_t step,size_t count) :
-            _beg(beg), _end(end), _step(step), _count(count) {}
+        Iter(size_t count,size_t step) :
+            _count(count), _step(step) {}
 
         Iter(size_t count) :
             _count(count) {}
@@ -54,12 +61,10 @@ struct LoopL {
         }
 
         bool operator==(const Iter& rhs) const {
-            //return _beg == rhs._beg;
             return _count == rhs._count;
         }
 
         bool operator!=(const Iter& rhs) const {
-            //return _beg != rhs._beg;
             return _count != rhs._count;
         }
 
@@ -69,11 +74,13 @@ struct LoopL {
     };
 
     Iter begin() const {
-        return Iter(_beg,_end,_step,_beg);
+        return Iter(_beg,_step);
     }
 
     Iter end() const {
-        return Iter(_beg + 1,_end,_step,_end);
+        size_t res = _end - _beg;
+        res = (res / _step) + ( (res % _step) == 0 ? 0 : 1 );
+        return Iter(_beg + res * _step);
     }
 
 };
