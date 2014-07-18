4ea98da94d5e726bfd040512997976551b6a4d7f
David <david@templatious.org>
2014-07-18 16:21:00 +0300

    dropped pointer version of std::vector
    specialization worked good.
    instHeap added to adapter standard.

--- adapters/StdVector.h
+++ adapters/StdVector.h
@@ -64,6 +64,17 @@ struct CollectionAdapter< std::vector<T,Alloc<T> > > {
 		return std::move(r);
 	}
 
+	static ThisCol* instHeap() {
+		return new ThisCol();
+	}
+
+	static ThisCol* instHeap(int size) {
+		ThisCol* r = new ThisCol();
+		r->reserve(size);
+		return r;
+	}
+
+
 	static iterator begin(ThisCol& c) {
 		return c.begin();
 	}
@@ -126,107 +137,6 @@ struct CollectionAdapter< std::vector<T,Alloc<T> > > {
         c.clear();
     }
 
-};
-
-template <class T,template <class> class Alloc>
-struct CollectionAdapter< std::vector<T,Alloc<T> >* > {
-
-    static const bool is_valid = true;
-
-	typedef typename std::vector<T, Alloc<T> >* ThisCol;
-    typedef typename std::vector<T, Alloc<T> > const* ConstCol;
-    typedef typename std::vector<T, Alloc<T> > ColType;
-	typedef typename ColType::iterator iterator;
-	typedef typename ColType::const_iterator const_iterator;
-	typedef T value_type;
-	typedef const T const_value_type;
-
-    template <class V>
-	static bool add(ThisCol c,V&& i) {
-		c->push_back(std::forward<V>(i));
-		return true;
-	}
-
-    template <class V>
-    static bool insert_at(ThisCol c, iterator at,V&& v) {
-        assert(at >= begin(c) && at < end(c));
-
-        c->insert(at,std::forward<V>(v));
-        return true;
-    }
-
-
-	static ThisCol instantiate() {
-		return new ColType();
-	}
-
-	static ThisCol instantiate(int size) {
-        ThisCol r = new ColType();
-		r->reserve(size);
-		return r;
-	}
-
-	static iterator begin(ThisCol c) {
-		return c->begin();
-	}
-
-	static iterator end(ThisCol c) {
-		return c->end();
-	}
-
-	static const_iterator begin(ConstCol c) {
-		return c->cbegin();
-	}
-
-	static const_iterator end(ConstCol c) {
-		return c->cend();
-	}
-
-	static const_iterator cbegin(ConstCol c) {
-		return c->cbegin();
-	}
-
-	static const_iterator cend(ConstCol c) {
-		return c->cend();
-	}
-
-	static size_t getSize(ConstCol c) {
-		return c->size();
-	}
-
-    static value_type& getByIndex(ConstCol c, int i) {
-        return (*c)[i];
-    }
-
-    static bool erase(ThisCol c,iterator beg) {
-        c->erase(beg);
-        return true;
-    }
-
-    static bool erase(ThisCol c,iterator beg,iterator end) {
-        c->erase(beg,end);
-        return true;
-    }
-
-    static value_type& first(ThisCol c) {
-        return c->front();
-    }
-
-    static const_value_type& first(ConstCol c) {
-        return c->front();
-    }
-
-    static value_type& last(ThisCol c) {
-        return c->back();
-    }
-
-    static const_value_type& last(ConstCol c) {
-        return c->back();
-    }
-
-    static void clear(ThisCol c) {
-        c->clear();
-    }
 };
 
 template <
