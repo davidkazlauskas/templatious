437d2070f4bfb051d4579b11b60caed6fde83bc2
David <david@templatious.org>
2014-09-14 21:11:57 +0300

    cleaned up, let's merge'

--- Pack.h
+++ Pack.h
@@ -42,15 +42,14 @@ struct Pack<A,Tail...> {
 
     template <class Ins,class... Args>
     struct InsertType {
-        //typedef Pack<Ins,A,typename TailPack::template InsertType<Ins>::value > value;
-        //typedef Pack<Args...,Ins,A> value;
-        typedef typename TailPack::template InsertType<Ins,Args...,Ins,A>::value value;
+        typedef typename TailPack::template InsertType<
+            Ins,Args...,Ins,A >::value value;
     };
 
     template <class Ins>
     struct FirstInsertType {
-        //typedef Pack<A,typename TailPack::template InsertType<Ins>::value > value;
-        typedef typename TailPack::template InsertType<Ins,A>::value value;
+        typedef typename TailPack::template InsertType<
+            Ins,A>::value value;
     };
 
     template <class ARef,class... TailRef>
@@ -60,7 +59,9 @@ struct Pack<A,Tail...> {
 
     template <class F,class... Args>
     void call(F&& f,Args&&... args) {
-        _t.call(std::forward<F>(f),std::forward<Args>(args)...,_r.getRef());
+        _t.call(std::forward<F>(f),
+                std::forward<Args>(args)...,
+                _r.getRef());
     }
 
     template <class F>
@@ -68,44 +69,25 @@ struct Pack<A,Tail...> {
         _t.call(std::forward<F>(f),_r.getRef());
     }
 
-    //template <class U,class T>
     template <class T>
     auto insert(T&& t)
-     //-> decltype(this->_t.insert(std::forward<T>(t),this->_r.getRef()))
-     //-> decltype(this->_t.insert(std::forward<T>(t),this->_r.getRef()))
      -> typename FirstInsertType<T>::value
     const {
          return _t.insert(std::forward<T>(t),_r.getRef());
     }
 
-    //template <class U,class T,class... Args>
     template <class T,class... Args>
-    //U insert(T&& t,Args&&... args)
     auto insert(T&& t,Args&&... args)
      -> typename InsertType<T,Args...>::value
-     //-> decltype(this->_t.insert(
-             //std::forward<T>(t),
-             //std::forward<Args>(args)...,
-             //std::forward<T>(t),
-             //this->_r.getRef())
-         //)
     const {
          return _t.insert(
                  std::forward<T>(t),
                  std::forward<Args>(args)...,
                  std::forward<T>(t),
                  _r.getRef());
-         //return _t.template insert<U>(
-                 //std::forward<T>(t),
-                 //std::forward<Args>(args)...,
-                 //std::forward<T>(t),
-                 //_r.getRef());
     }
 
-    //templatious::util::RefContainer<A> _r;
-    //Pack<Tail...> _t;
 private:
-    //templatious::util::RefContainer<A> _r;
     Container _r;
     Pack<Tail...> _t;
 };
@@ -121,7 +103,6 @@ struct Pack<A> {
 
     typedef Pack<A> ThisPack;
 
-    //template <class Ins>
     template <class Ins,class... Args>
     struct InsertType {
         typedef Pack<Args...,Ins,A> value;
@@ -131,7 +112,6 @@ struct Pack<A> {
     struct FirstInsertType {
         typedef ThisPack value;
     };
-
 
     template <class ARef>
     explicit Pack(ARef&& r)
@@ -147,28 +127,15 @@ struct Pack<A> {
         return ThisPack(_r.getRef());
     }
 
-    //template <class U,class T,class... Args>
     template <class T,class... Args>
     auto insert(T&& t,Args&&... args)
-     //-> Pack<Args...,decltype(t),decltype(this->_r.getRef())>
      -> typename InsertType<T,Args...>::value
     const {
-         //return Pack<
-             //Args...,
-             //decltype(t),
-             ////decltype(this->_r.getRef())
-             //A
-             //>(
-                    //std::forward<Args>(args)...,
-                    //std::forward<T>(t),
-                    //_r.getRef()
-                 //);
         typedef typename InsertType<T,Args...>::value RetType;
         return RetType(std::forward<Args>(args)...,
                 std::forward<T>(t),_r.getRef());
     }
 
-    //templatious::util::RefContainer<A> _r;
 private:
     Container _r;
 };
