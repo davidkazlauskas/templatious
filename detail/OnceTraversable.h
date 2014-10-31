54479f6de88d0861070ab21418b266da40aebe78
David <david@templatious.org>
2014-10-31 15:52:58 +0200

    new class

---
+++ detail/OnceTraversable.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  OnceTraversable.h
+ *
+ *    Description:  Once traversable collection wrapper that mutates iterators
+ *
+ *        Version:  1.0
+ *        Created:  10/31/2014 03:52:22 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#ifndef ONCETRAVERSABLE_7K4ACVNE
+#define ONCETRAVERSABLE_7K4ACVNE
+
+
+
+#endif /* end of include guard: ONCETRAVERSABLE_7K4ACVNE */
