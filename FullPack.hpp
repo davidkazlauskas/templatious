c4ee744e6ff68c54e2ac49f747217ab074f88176
David <david@templatious.org>
2015-03-16 16:47:10 +0200

    full pack

---
+++ FullPack.hpp
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  FullPack.hpp
+ *
+ *    Description:  Everything with adapters
+ *
+ *        Version:  1.0
+ *        Created:  2015.03.16 16:46:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#include <templatious/MainPack.hpp>
+#include <templatious/adapters/All.hpp>
+
