68dea99c40691ff06f4b12120a18b9b84c86b21a
David <david@templatious.org>
2015-03-16 16:44:39 +0200

    mainpack file

---
+++ MainPack.hpp
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  MainPack.hpp
+ *
+ *    Description:  Main functions without adapters
+ *
+ *        Version:  1.0
+ *        Created:  2015.03.16 16:43:34
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#include <templatious/StaticAdapter.hpp>
+#include <templatious/StaticFactory.hpp>
+#include <templatious/StaticManipulator.hpp>
+
+#include <templatious/Sugar.hpp>
